2632cf14005bf5e6464a1a8e0e866492aa45fe9c
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-24 18:14:09 +0530

    commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/products.dart';
+
+class ProductItem extends StatelessWidget {
+  // const ProductItem({super.key});
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id,this.title,this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(child: Image.network('src'),);
+  }
+}
