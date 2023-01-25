7ce2a29684397a2beca033253b114ae44e6934f3
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-25 19:05:19 +0530

    commit

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -12,6 +12,7 @@ class PruductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
