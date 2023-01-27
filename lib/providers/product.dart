77d98248da1150158ab26397d729e54f967ff03f
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-27 18:46:09 +0530

    commit

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -17,4 +17,10 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
+
+  void toggleFavoriteStatus()
+  {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
