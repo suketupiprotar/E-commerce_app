621c7a0f28ad16c03cb32673ab5495009679ddc8
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-28 12:33:56 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/cart.dart';
 import './screens/products_overview_screen.dart';
 import 'package:shop_app/screens/product_details_screen.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) =>  Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       // create: (ctx) => Products(),
       child: MaterialApp(
         title: 'MyShop',
