a4d373b7d862e7da1de5c2d0993b37dcaa9464e7
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-28 12:28:20 +0530

    commit

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -7,24 +7,46 @@ class CartItem {
   final int quantity;
   final double price;
 
-  CartItem(
-    this.id,
-    this.title,
-    this.quantity,
-    this.price,
-  );
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
 }
 
 class Cart with ChangeNotifier {
-  late Map<String,CartItem> _items;
+  late Map<String, CartItem> _items;
 
-  Map<String,CartItem> get items {
-    return {..._items}; 
+  Map<String, CartItem> get items {
+    return {..._items};
   }
 
-  void addItem(String productId,String price,String title,)
-  {
-    if(_items.containsKey(key))
+  void addItem(
+    String productId,
+    double price,
+    String title,
+  ) {
+    if (_items.containsKey(productId)) {
+      //change quantity
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
   }
-
 }
