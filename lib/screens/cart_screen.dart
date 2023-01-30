65518073a5541362d5bc9b6d7ef15ae445da3f6b
suketupiprotat <suketupiprotar9930@gmail.com>
2023-01-30 21:05:33 +0530

    commit

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -15,38 +15,42 @@ class CartScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Your Cart'),
       ),
-      body: Column(children: [
-        Card(
-          margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Total',
-                  style: TextStyle(fontSize: 20),
-                ),
-                Spacer(),
-                Chip(
-                  label: Text(
-                    '\$${cart.totalAmount}',
-                    style: TextStyle(color: Colors.white),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
                   ),
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                TextButton(
-                  child: Text(
-                    'ORDER NOW',
-                    style: TextStyle(color: Theme.of(context).primaryColor),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  onPressed: () {},
-                )
-              ],
+                  TextButton(
+                    child: Text(
+                      'ORDER NOW',
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
           ),
-        )
-      ]),
+          SizedBox(height: 10,),
+          Expanded(child: ListView.builder(itemCount: cart.itemCount,itemBuilder: (ctx,i) => ,),),
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class CartItem extends StatelessWidget {
+  const CartItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 4,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(18),
+        child: ListTile(
+          leading: ,
+        ),
+      ),
+    );
+  }
+}
