a0fbf5255d06cfe1448d9e916d7f617898a7b59d
suketupiprotat <suketupiprotar9930@gmail.com>
2023-02-02 18:19:49 +0530

    commit

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/orders.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: ListView.builder(itemCount: orderData.orders.length,itemBuilder: (ctx,i) => ,),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,35 @@
+import 'dart:html';
+
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:shop_app/providers/orders.dart' as ord;
+import 'package:intl/intl.dart';
+
+class OrderItem extends StatelessWidget {
+  // const OrderItem({super.key});
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
